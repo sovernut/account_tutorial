01923424ca40fad491afe82bf6a26a06658a97c6
Nuttaphon Rodphakdeekul <eparis@atlassian.com>
2017-03-09 20:31:27 +0700

    Tutorial 2 add account and create first view

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Account,Transaction
+
+admin.site.register(Account)
+admin.site.register(Transaction)
--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Account,Transaction
 
 def index(request):
-    return HttpResponse("this is simple text")
+    content = "Testcontent"
+    return render(request,"account/index.html",{"contentinHTML":content})
