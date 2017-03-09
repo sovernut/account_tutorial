41cd524c78c3cead00d37f6bf16bf65eb8a334f8
Nuttaphon Rodphakdeekul <eparis@atlassian.com>
2017-03-09 20:46:50 +0700

    Tutorial 2 create detail view

--- account/views.py
+++ account/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 from .models import Account,Transaction
 
 def index(request):
-    content = "Testcontent"
-    return render(request,"account/index.html",{"contentinHTML":content})
+    account = Account.objects.order_by('-account_name')
+    return render(request,"account/index.html",{"account":account})
+    
+def detail(request,account_id):
+    account = Account.objects.get(pk=account_id)
+    return render(request,"account/detail.html",{"account":account})
